7e4969a14a7ef08a2e3d1ac07f31385d14a1de9e
Jiatai Han <hjt486@gmail.com>
2019-10-20 19:36:39 -0500

    First commit

---
+++ MyGithubLists/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Octokit;
+
+namespace MyGithubLists.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public async System.Threading.Tasks.Task<ActionResult> Index()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("MyGithubLists"));
+            var basicAuth = new Credentials("You Github Username", "You Github Password"); // NOTE: Replace with your GitHub username and password
+            client.Credentials = basicAuth;
+            var user = await client.User.Current();
+
+            SearchRepositoryResult results = await client.Search.SearchRepo(
+                new SearchRepositoriesRequest("stars%3A>0")
+                {
+                    SortField = RepoSearchSort.Stars
+                });
+
+            string[,] results_array = new string[25, 6];
+            for (int i = 0; i < 25; i++)
+            {
+                results_array[i, 0] = results.Items[i].FullName;
+                results_array[i, 1] = results.Items[i].StargazersCount.ToString();
+                results_array[i, 2] = results.Items[i].HtmlUrl;
+                results_array[i, 3] = results.Items[i].Owner.Login;
+                results_array[i, 4] = results.Items[i].CreatedAt.ToString();
+                results_array[i, 5] = results.Items[i].Description;
+            }
+
+            return View(results_array);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+# MyGithubLists
+
+This is a .Net MVC 5.0 Program to pull the most starred repositories from Github, using official Github API and Octokit.net (a GitHub API Client Library for .NET) . This program contains no Models since it's simple enough to use only Controller and Views.
+
+
+
+This is built for **Interactive Take Home Assignment**.
+
+
+
+Instruction:
+
+This program is built with **Microsoft Visual Studio Community 2015**, Before you build the project, 
+
+1. PLEASE open the **MyGithubLists/Controllers/HomeController.cs**;
+
+2. Find **Line 16**;
+
+3. Replace **"You Github Username"** with your own Github username, and **"You Github Password"** with your own Github password:
+
+	```c#
+	var basicAuth = new Credentials("You Github Username", "You Github Password"); 
+	// NOTE: Replace with your GitHub username and password
+	```
+	
+4. Build and run the project.
+
+
+
+Thank you,
+
+
+
+Jiatai Han
